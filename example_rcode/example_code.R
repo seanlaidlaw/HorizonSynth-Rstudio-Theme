e1474faef6b13a6ad0425f73793276e91f4fd334
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2021-03-14 22:41:34 +0000

    add Rstudio example code to demonstrate theme

---
+++ example_rcode/example_code.R
@@ -0,0 +1,29 @@
+# plotting of R objects
+plot <- function (x, y, ...)
+{
+  if (is.function(x) && 
+      is.null(attr(x, "class")))
+  {
+    if (missing(y))
+      y <- NULL
+    
+    # check for ylab argument
+    hasylab <- function(...) 
+      !all(is.na(
+        pmatch(names(list(...)),
+              "ylab")))
+    
+    if (hasylab(...))
+      plot.function(x, y, ...)
+    
+    else 
+      plot.function(
+        x, y, 
+        ylab = paste(
+          deparse(substitute(x)),
+          "(x)"), 
+        ...)
+  }
+  else 
+    UseMethod("plot")
+}
